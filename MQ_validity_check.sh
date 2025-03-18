71785be4ac2e30091db9e71e134badb0e07496b5
w7Pratham <prathamshingne@gmail.com>
2025-03-18 11:50:17 +0530

    MQ validity check reworked

--- MQ_validity_check.sh
+++ MQ_validity_check.sh
@@ -6,7 +6,25 @@ current_user=$(whoami)
 # Check if the user is arther or jasper
 if [ "$current_user" == "arther" ] || [ "$current_user" == "jasper" ]; then
   echo "User is $current_user. Running the next part of the script..."
-  # Add the next part of your script here
+  
+  # Check if the folder with the same name exists in ~/Project/WORK/
+  user_folder="$HOME/Project/WORK/$current_user"
+  if [ -d "$user_folder" ]; then
+    echo "Folder $user_folder exists."
+    
+    # Check if listed.kdb file exists in the folder
+    if [ -f "$user_folder/listed.kdb" ]; then
+      echo "Yes, listed.kdb file is present in $user_folder."
+      
+      # Grep the date from the To: field
+      to_date=$( "$user_folder/listed.kdb")
+      echo "The date from the To: field is: $to_date"
+    else
+      echo "No, listed.kdb file is not present in $user_folder."
+    fi
+  else
+    echo "Folder $user_folder does not exist."
+  fi
 else
   echo "User is not arther or jasper. Exiting."
   exit 1
