8087ecc521eb5f91dcd04a1a000f4fc2b94912bc
w7Pratham <prathamshingne@gmail.com>
2025-03-18 11:43:55 +0530

    status check for BG working

--- de_status_check.sh
+++ de_status_check.sh
@@ -2,17 +2,20 @@
 
 # Get the current username
 current_user=$(whoami)
+echo "Debug: Current user is $current_user"
 
 # Check if the user is arther or jasper
 if [ "$current_user" == "shiv" ] || [ "$current_user" == "jasper" ]; then
   echo "User is $current_user. Running the next part of the script..."
   
   # Extract the Status column from the file
-  statuses=$(cat /home/shiv/Project/WORK/arther/status_check_data.txt | awk -F"|" 'NR>2{print $3}' )
+  statuses=$(awk 'NR>2 {print $2}' /home/shiv/Project/WORK/arther/status_check_data.txt)
+  echo "Debug: Extracted statuses are: $statuses"
 
   # Check if all statuses are START
   all_start=true
   for status in $statuses; do
+    echo "Debug: Checking status: $status"
     if [ "$status" != "START" ]; then
       all_start=false
       break
